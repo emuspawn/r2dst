d0afe317d52ccc6f9359348bc02112dcf39d7d49
americaneseia <americaneseia@621c9c31-3b52-0410-9324-6feef30e6307>
2008-07-18 05:06:41 +0000

    separator button addition

---
+++ trunk/SimpleWar4/ui/button/Separator.java
@@ -0,0 +1,15 @@
+package ui.button;
+
+import world.World;
+
+public class Separator extends Button
+{
+	public Separator(int x, int y, int width, int height)
+	{
+		super("-------", x, y, width, height);
+	}
+	public void performAction(World w)
+	{
+		
+	}
+}
