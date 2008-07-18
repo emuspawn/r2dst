cd4ca2f882933e99d7ba1315a87a6254c21c5a5d
americaneseia <americaneseia@621c9c31-3b52-0410-9324-6feef30e6307>
2008-07-18 04:57:26 +0000

    added menu buttons for MainEditMenu

---
+++ trunk/SimpleWar4/ui/button/editButtons/mainEditMenuButtons/EditTerrainButton.java
@@ -0,0 +1,16 @@
+package ui.button.editButtons.mainEditMenuButtons;
+
+import ui.button.*;
+import world.World;
+
+public class EditTerrainButton extends Button
+{
+	public EditTerrainButton(int x, int y, int width, int height)
+	{
+		super("Edit Terrain", x, y, width, height);
+	}
+	public void performAction(World w)
+	{
+		
+	}
+}
