f9306d9d20099af23b7fee8209079dbe21ed89cc
aicommander <aicommander@621c9c31-3b52-0410-9324-6feef30e6307>
2008-12-20 15:41:11 +0000

    Finish object sending functionality. Now fully tested and working.

--- trunk/networkingLib/src/Sendable.java
+++ trunk/networkingLib/src/Sendable.java
@@ -1,6 +1,8 @@
+import java.util.ArrayList;
+
 
 public interface Sendable {
-	//This must return a string which can be passed to the constructor of the object
+	//This must return an array list of strings which can be passed to the constructor of the object
 	//to recreate the object
-	public String toSendString();
+	public ArrayList<String> toSendStringList();
 }
