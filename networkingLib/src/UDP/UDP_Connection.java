2a8a8d2c7575ec5e581692f9c1560f3d9dd68b10
aicommander <aicommander@621c9c31-3b52-0410-9324-6feef30e6307>
2009-02-03 17:27:38 +0000

    - Remove UDP_Lossless (it fails) and rewrite UDP_Connection

--- networkingLib/src/UDP/UDP_Connection.java
+++ networkingLib/src/UDP/UDP_Connection.java
@@ -1,139 +1,40 @@
 package UDP;
-import java.util.ArrayList;
-import java.util.LinkedList;
 import java.io.IOException;
 import java.net.*;
 
 
 public class UDP_Connection extends Thread {
-	private LinkedList<DatagramPacket> datagrams;
-	private ArrayList<SocketAddress> connections;
-	private DatagramSocket sock;
-	private int maxBufferSize;
+	private DatagramSocket dsock;
 	
-	private final byte cmdNormal = 0;
-	private final byte cmdConnect = 1;
-	private final byte cmdDisconnect = 2;
-	
-	public UDP_Connection(int port, int maxData) throws SocketException
+	public UDP_Connection(int prt) throws SocketException
 	{
-		sock = new DatagramSocket(port);
-		connections = new ArrayList<SocketAddress>();
-		datagrams = new LinkedList<DatagramPacket>();
-		maxBufferSize = maxData;
-		
-		start();
+		dsock = new DatagramSocket(prt);
 	}
 	
-	public boolean connectTo(InetAddress addr, int port)
+	public DatagramPacket receiveDatagram(int packetSize)
 	{
-		byte[] buffer = new byte[maxBufferSize];
-		DatagramPacket datagram = new DatagramPacket(buffer, buffer.length, addr, port);
-		setupPacket(datagram, cmdConnect);
+		DatagramPacket pack = new DatagramPacket(new byte[packetSize], packetSize);
 		try {
-			sock.send(datagram);
-			connections.add(datagram.getSocketAddress());
+			dsock.receive(pack);
+		} catch (IOException e) {
+			return null;
+		}
+		return pack;
+	}
+	
+	public boolean sendDatagram(DatagramPacket pack)
+	{
+		try {
+			dsock.send(pack);
 			return true;
 		} catch (IOException e) {
-			e.printStackTrace();
 			return false;
 		}
 	}
 	
-	public boolean disconnect(InetAddress addr, int port)
+	public boolean close()
 	{
-		byte[] buffer = new byte[maxBufferSize];
-		DatagramPacket datagram = new DatagramPacket(buffer, buffer.length, addr, port);
-		setupPacket(datagram, cmdDisconnect);
-		try {
-			sock.send(datagram);
-			connections.remove(datagram.getSocketAddress());
-			return true;
-		} catch (IOException e) {
-			e.printStackTrace();
-			return false;
-		}
-	}
-	
-	public ArrayList<SocketAddress> getConnections()
-	{
-		return new ArrayList<SocketAddress>(connections);
-	}
-	
-	public void run()
-	{
-		while (!isInterrupted())
-		{
-			DatagramPacket datagram = new DatagramPacket(new byte[maxBufferSize+1], maxBufferSize+1);
-			try {
-				sock.receive(datagram);
-			} catch (IOException e) { 
-				e.printStackTrace();
-				return; 
-			}
-			
-			if (datagram.getData()[0] == cmdConnect)
-			{
-				connections.add(datagram.getSocketAddress());
-			}
-			else if (datagram.getData()[0] == cmdDisconnect)
-			{
-				connections.remove(datagram.getSocketAddress());
-			}
-			else
-			{
-				datagrams.add(datagram);
-			}
-		}
-	}
-	
-	public byte[] recv(boolean block)
-	{
-		byte[] data = null, newData = new byte[maxBufferSize];
-		if (!datagrams.isEmpty())
-		{
-			data = datagrams.poll().getData();
-			
-			for (int i = 1; i < data.length; i++)
-				newData[i-1] = data[i];
-		} 
-		else if (block)
-		{
-			while (datagrams.isEmpty());
-			
-			data = datagrams.poll().getData();
-			
-			for (int i = 1; i < data.length; i++)
-				newData[i-1] = data[i];
-		}
-		
-		return newData;
-	}
-	
-	public boolean send(InetAddress addr, int port, byte[] buffer)
-	{
-		DatagramPacket datagram = new DatagramPacket(buffer, buffer.length, addr, port);
-		
-		try {
-			setupPacket(datagram, cmdNormal);
-			sock.send(datagram);
-			return true;
-		} catch (IOException e) {
-			e.printStackTrace();
-			return false;
-		}
-	}
-	private void setupPacket(DatagramPacket datagram, byte packetFlag)
-	{
-		byte[] newData = new byte[datagram.getData().length + 1];
-		
-		newData[0] = packetFlag;
-		
-		for (int i = 1; i < newData.length; i++)
-		{
-			newData[i] = datagram.getData()[i-1];
-		}
-		
-		datagram.setData(newData);
+		dsock.close();
+		return true;
 	}
 }
