c8adc934eaedb1a64096c56b557a3c10991465f3
aicommander <aicommander@621c9c31-3b52-0410-9324-6feef30e6307>
2009-04-27 20:49:53 +0000

    - Epic UDP implementation  - Uses callbacks to deliver packets so we don't have to poll for packets and use 100% CPU

---
+++ trunk/networkingLib/src/UDP/UDP_Callbacks.java
@@ -0,0 +1,23 @@
+package UDP;
+
+import java.net.DatagramPacket;
+
+public abstract class UDP_Callbacks {
+	private int callbackVer = -1;
+	
+	//A packet has been received on the socket
+	public abstract void PacketReceived(DatagramPacket pack);
+	
+	//The socket has caused an exception to be thrown
+	//when attempting to receive
+	//!!! THE SOCKET IS DEAD WHEN THIS IS CALLED !!!
+	public abstract void ReceiveException(Exception e);
+	
+	public void setCallbackVersion(int callVer) {
+		callbackVer = callVer;
+	}
+	
+	public int getCallbackVersion() {
+		return callbackVer;
+	}
+}
--- trunk/networkingLib/src/UDP/UDP_Connection.java
+++ trunk/networkingLib/src/UDP/UDP_Connection.java
@@ -1,40 +1,54 @@
 package UDP;
-import java.io.IOException;
 import java.net.*;
 
 
-public class UDP_Connection extends Thread {
+public class UDP_Connection implements Runnable {
 	private DatagramSocket dsock;
+	private UDP_Callbacks callbacks;
+	private int maxSize;
+	private Thread thrd;
 	
-	public UDP_Connection(int prt) throws SocketException
+	public UDP_Connection(int prt, UDP_Callbacks calls, int maxPacketSize) throws SocketException
 	{
+		//Validate that the caller set the callback version number
+		if (calls.getCallbackVersion() == -1)
+			throw new IllegalArgumentException("Invalid callback version number");
+		
 		dsock = new DatagramSocket(prt);
+		callbacks = calls;
+		maxSize = maxPacketSize;
+		
+		//Create our receive thread and start it
+		thrd = new Thread(this);
+		thrd.start();
 	}
 	
-	public DatagramPacket receiveDatagram(int packetSize)
+	public void run()
 	{
-		DatagramPacket pack = new DatagramPacket(new byte[packetSize], packetSize);
-		try {
-			dsock.receive(pack);
-		} catch (IOException e) {
-			return null;
+		while (!thrd.isInterrupted())
+		{
+			DatagramPacket pack = new DatagramPacket(new byte[maxSize], maxSize);
+			
+			try {
+				//Receive the packet
+				dsock.receive(pack);
+				
+				//If we receive without throwing an exception then forward
+				//the packet to the PacketReceived handler
+				callbacks.PacketReceived(pack);
+				
+			} catch (Exception e) {
+				//Forward the exception to the ReceiveException handler
+				//so it can determine what to do
+				callbacks.ReceiveException(e);
+				return;
+			}
 		}
-		return pack;
 	}
 	
-	public boolean sendDatagram(DatagramPacket pack)
+	//Returns the underlying datagram socket
+	public DatagramSocket getSocket()
 	{
-		try {
-			dsock.send(pack);
-			return true;
-		} catch (IOException e) {
-			return false;
-		}
-	}
-	
-	public boolean close()
-	{
-		dsock.close();
-		return true;
+		return dsock;
 	}
 }
