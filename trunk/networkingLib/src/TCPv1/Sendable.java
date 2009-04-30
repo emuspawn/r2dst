ff9b4a11f145282ecfdc672bef70dd19f7abf057
aicommander <aicommander@621c9c31-3b52-0410-9324-6feef30e6307>
2009-04-30 04:11:10 +0000

    - SVN cleanup

--- trunk/networkingLib/src/TCPv1/Sendable.java
+++
@@ -1,9 +0,0 @@
-package TCPv1;
-import java.util.ArrayList;
-
-
-public interface Sendable {
-	//This must return an array list of strings which can be passed to the constructor of the object
-	//to recreate the object
-	public ArrayList<String> toSendStringList();
-}
