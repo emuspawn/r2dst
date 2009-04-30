41830c1dbd80f6030b9561c8ee57baa21064cd62
aicommander <aicommander@621c9c31-3b52-0410-9324-6feef30e6307>
2009-04-30 05:32:08 +0000

    - Many fixes  - I don't really feel like going into any more depth than that

--- trunk/networkingLib/src/TCP/TCP_Client.java
+++ trunk/networkingLib/src/TCP/TCP_Client.java
@@ -1,26 +1,20 @@
 package TCP;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 
 public class TCP_Client implements Runnable {
 	private Socket sock;
 	
-	private BufferedInputStream in;
-	private BufferedOutputStream out;
-	
 	private TCP_Client_Callbacks callbacks;
 	
 	public TCP_Client(InetAddress addr, int port, TCP_Client_Callbacks calls) throws IOException
 	{
 		sock = new Socket(addr, port);
-		in = new BufferedInputStream(sock.getInputStream());
-		out = new BufferedOutputStream(sock.getOutputStream());
 		callbacks = calls;
-		new Thread().start();
+		new Thread(this).start();
 	}
 	
 	public Socket getSocket()
@@ -28,34 +22,25 @@ public class TCP_Client implements Runnable {
 		return sock;
 	}
 	
-	//The returned list is read-only
-	public BufferedInputStream getInStream()
-	{
-		return in;
-	}
-	
-	//The returned list is read-only
-	public BufferedOutputStream getOutStream()
-	{
-		return out;
-	}
-	
 	public void run()
 	{
 		for (;;)
 		{
 			byte[] buffer;
+			InputStream in;
 			try {
+				in = sock.getInputStream();
+				
 				if (in.available() == 0)
 					continue;
 				
 				buffer = new byte[in.available()];
 				in.read(buffer);
-					
+
 				callbacks.DataReceived(buffer);
 			} catch (IOException e) {
 				callbacks.ReceiveException(e);
-				break;
+				return;
 			}
 		}	
 	}
